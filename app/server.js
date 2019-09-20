b03978400f165bb588e85d35a2b57fdaa42c0eb3
Tabatha Poonpurmsiri <tabatha.pp@gmail.com>
2019-09-20 10:45:50 -0400

    updated friendsArray

--- app/server.js
+++ app/server.js
@@ -2,22 +2,25 @@ var express = require("express");
 var path = require("path");
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "view.html"));
-  });
-  
-  app.get("/add", function(req, res) {
-    res.sendFile(path.join(__dirname, "add.html"));
-  });
-  
- 
 
-  app.listen(PORT, function() {
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "view.html"));
+});
+
+app.get("/add", function (req, res) {
+    res.sendFile(path.join(__dirname, "add.html"));
+});
+
+
+
+app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
-  });
-  
+});
