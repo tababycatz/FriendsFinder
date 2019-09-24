0605d6e39e3582768bb4f407df205ae27c94b5ba
Tabatha Poonpurmsiri <tabatha.pp@gmail.com>
2019-09-23 21:38:50 -0400

    working on loop

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,5 +9,26 @@ module.exports = function(app) {
     app.post("/api/friends", function(req, res){
         friendsData.push(req.body);
         res.json(true);
+
+        var newFriend = req.body;
+        var friendAnswer = newFriend.scores;
+        var matchName = '';
+        var matchPic = '';
+        var totalDifference = 1000;
+
+        for (let i = 0; i < friendsData.length; i++) {
+            var diff = 0;
+            for (let j = 0; j < friendAnswer.length; j++) {
+                diff += Math.abs(friends[i].scores[j] - userResponses[j]);
+            }
+            if (diff < totalDifference) {
+                totalDifference = diff;
+                matchName = friendsData[i].name;
+                matchPic = friendsData[i].photo;
+            }
+        };
+
+        friendsData.push(newFriend);
+        res.json({status: 'OK', matchName: matchName, matchPic: matchPic});
     })
 };
