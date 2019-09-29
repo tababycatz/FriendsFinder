604a6ad9555af11f9e73a27cd71976e13be38170
Tabatha Poonpurmsiri <tabatha.pp@gmail.com>
2019-09-29 15:04:01 -0400

    updated friendsArray w/ more ppl

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,27 @@ var friendArray = [
     {
         friendName: 'Robert',
         friendPhoto: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiT4uWt0d_kAhWurFkKHevQCsQQjRx6BAgBEAQ&url=%2Furl%3Fsa%3Di%26source%3Dimages%26cd%3D%26ved%3D%26url%3Dhttps%253A%252F%252Fwww.shutterstock.com%252Fsearch%252Fplaceholder%26psig%3DAOvVaw2lUld1WCt3qT-wG9YAVUXy%26ust%3D1569076970708635&psig=AOvVaw2lUld1WCt3qT-wG9YAVUXy&ust=1569076970708635example@example.com',
-        scores: ["1", "2", "4", "5", "5", "3", "2", "1","5", "4"]
+        scores: ["1", "2", "4", "5", "5", "3", "2", "1", "5", "4"]
+    },
+    {
+        friendName: 'Jonathan',
+        friendPhoto: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiT4uWt0d_kAhWurFkKHevQCsQQjRx6BAgBEAQ&url=%2Furl%3Fsa%3Di%26source%3Dimages%26cd%3D%26ved%3D%26url%3Dhttps%253A%252F%252Fwww.shutterstock.com%252Fsearch%252Fplaceholder%26psig%3DAOvVaw2lUld1WCt3qT-wG9YAVUXy%26ust%3D1569076970708635&psig=AOvVaw2lUld1WCt3qT-wG9YAVUXy&ust=1569076970708635example@example.com',
+        scores: ["4", "3", "2", "1", "5", "1", "2", "3", "4", "5"]
+    },
+    {
+        friendName: 'Lisa',
+        friendPhoto: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiT4uWt0d_kAhWurFkKHevQCsQQjRx6BAgBEAQ&url=%2Furl%3Fsa%3Di%26source%3Dimages%26cd%3D%26ved%3D%26url%3Dhttps%253A%252F%252Fwww.shutterstock.com%252Fsearch%252Fplaceholder%26psig%3DAOvVaw2lUld1WCt3qT-wG9YAVUXy%26ust%3D1569076970708635&psig=AOvVaw2lUld1WCt3qT-wG9YAVUXy&ust=1569076970708635example@example.com',
+        scores: ["2", "3", "4", "5", "1", "2", "3", "4", "5", "4"]
+    },
+    {
+        friendName: 'Abigail',
+        friendPhoto: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiT4uWt0d_kAhWurFkKHevQCsQQjRx6BAgBEAQ&url=%2Furl%3Fsa%3Di%26source%3Dimages%26cd%3D%26ved%3D%26url%3Dhttps%253A%252F%252Fwww.shutterstock.com%252Fsearch%252Fplaceholder%26psig%3DAOvVaw2lUld1WCt3qT-wG9YAVUXy%26ust%3D1569076970708635&psig=AOvVaw2lUld1WCt3qT-wG9YAVUXy&ust=1569076970708635example@example.com',
+        scores: ["1", "4", "4", "3", "5", "5", "4", "4", "1", "1"]
+    },
+    {
+        friendName: 'Sam',
+        friendPhoto: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiT4uWt0d_kAhWurFkKHevQCsQQjRx6BAgBEAQ&url=%2Furl%3Fsa%3Di%26source%3Dimages%26cd%3D%26ved%3D%26url%3Dhttps%253A%252F%252Fwww.shutterstock.com%252Fsearch%252Fplaceholder%26psig%3DAOvVaw2lUld1WCt3qT-wG9YAVUXy%26ust%3D1569076970708635&psig=AOvVaw2lUld1WCt3qT-wG9YAVUXy&ust=1569076970708635example@example.com',
+        scores: ["3", "4", "5", "2", "5", "1", "4", "3", "2", "4"]
     }
 ];
 
