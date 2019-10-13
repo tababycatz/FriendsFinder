96868c1512b6224e20906222667c5969300fabc0
Tabatha Poonpurmsiri <tabatha.pp@gmail.com>
2019-10-13 12:19:32 -0400

    updating

--- public/assets/js/finder.js
+++ public/assets/js/finder.js
@@ -1,17 +1,4 @@
-$('modal-trigger').leanModal();
-
-$("#submit").on("click", function (event) {
-		if ( $('#name').val() == null || $('#photo').val() == null ) {
-			alert("Oops! You're missing something, please fill out the form.");
-			return false;
-		};
-
-		for (var i = 0; i < 10; i++) {
-			if ( $("input:radio[name=question"+i+"]:checked").val() == null ) {
-				alert("Please fill out all questions!");
-				return false;
-			};
-		};
+$("#submitBtn").on("click", function() {
     
         var newFriend = {
         name: $("#name").val(),
@@ -30,29 +17,15 @@ $("#submit").on("click", function (event) {
                 ]
     };
 
-    $.post("/api/friends", newFriend, function (data) {
-        
-        $("#friend-match-name").text(data.matchName);
-        $("#friend-match-pic").attr("src", data.matchPic);
-        
-        $("#results").modal("toggle");
+
+    $.post('/api/friends', newFriend).done(function(data) {
+        $('#myModal').openModal();
+        $('#modalBody').append("<h5>" + data.name)
+        $('#modalBody').append(data.photo);
         
     });  
-});  
 
-// Storing our current URL in a variable.
-var currentURL = window.location.origin;
-
-// Making an AJAX post request. The server calculates the match and sends back the info in the data object.
-$.post(currentURL + '/api/survey', person).done(function(data){
-
-    // Opens our modal.
-    $('#modal1').openModal();
-
-    // Populate out modal with information about the match.
-    $('#modalArea').empty()
-    var picture = '<img class="circle" src="'+data.image+'"">';
-    $('#modalArea').append("<h5>" + data.name + " !</h5>")
-    $('#modalArea').append(picture)
-
+    console.log(newFriend)
 });
+
+
